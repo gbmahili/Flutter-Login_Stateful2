78c0e19208f8e2e45811afd88dd65f604e2442b0
Baraka Mahili <georgesmahili@gmail.com>
2019-05-23 19:43:45 -0400

    Added post, get and an express backend server

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() {
   runApp(App());
@@ -11,7 +13,16 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final formKey = GlobalKey<FormState>();
-  String username = '', password = '';
+  String username = '', password = '', name = '', origin = '', errorField='';
+  // HTTP:
+  // Future<String> makeRequest() async {
+  //   String url = "https://randomuser.me/api/";
+  //   var response =
+  //       await http.get(Uri.encodeFull(url), headers: {'Accept': 'application/json'});
+  //       print(response.body);
+  //   return response.body;
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,13 +64,74 @@ class _AppState extends State<App> {
                   'Submit',
                   style: TextStyle(color: Colors.white),
                 ),
-                onPressed: () {
+                onPressed: () async {
                   formKey.currentState.validate();
                   formKey.currentState.save();
                   print('Username is: $username and password is: $password');
+                  // GET REQUEST:
+                  // Create a function up there and use it below like this:
+                  // var r = makeRequest();
+                  //print(r);
+
+                  // String expressGETBackendUrl = "http://localhost:3030/getData";
+                  // var response = await http.get(
+                  //   Uri.encodeFull(expressGETBackendUrl),
+                  //   headers: {'Accept': 'application/json'},
+                  // );
+                  // var jsonResponse = convert.jsonDecode(response.body);
+                  // setState(() {
+                  //   name = jsonResponse['name'];
+                  //   origin = jsonResponse['from'];
+                  // });
+
+                  //  POST REQUEST:
+                  if (formKey.currentState.validate()) {
+                    String expressPOSTBackendUrl =
+                        "http://localhost:3030/flutter-login";
+                    Map userInfo = {"username": username, "password": password};
+
+                    var reponsePOST = await http.post(
+                      expressPOSTBackendUrl,
+                      headers: {'Accept': 'application/json'},
+                      body: userInfo,
+                    );
+                    var postResponseJson = convert.jsonDecode(reponsePOST.body);
+                    print(postResponseJson);
+                    if (reponsePOST.statusCode == 200) {
+                      setState(() {
+                        name = postResponseJson['name'];
+                        origin = postResponseJson['from'];
+                        errorField = '';
+                      });
+                    } else {
+                      setState(() {
+                        name = '';
+                        origin = '';
+                        errorField = postResponseJson['error'];
+                      });
+                      
+                    }
+                  }
+
+                  // http.get(
+                  //   Uri.encodeFull(expressBackendUrl),
+                  //   headers: {'Accept': 'application/json'},
+                  // ).then((response) {
+                  //   print(response.body);
+                  //   var jsonResponse = convert.jsonDecode(response.body);
+                  //   setState(() {
+                  //     name = jsonResponse['name'];
+                  //     origin = jsonResponse['from'];
+                  //     print(jsonResponse['name']);
+                  //     print(jsonResponse['from']);
+                  //   });
+                  // });
                 },
                 color: Colors.blueAccent,
-              )
+              ),
+              name.length > 0 ? Text('Name: $name') : Text(''),
+              origin.length > 0 ? Text('Origin: $origin') : Text(''),
+              errorField.length > 0 ? Text('Error: $errorField') : Text(''),
             ],
           ),
         ),
--- server/index.js
+++ server/index.js
@@ -7,6 +7,20 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3030;
 
 app.post('/flutter-login', (req, res) => {
-    console.log(req.body);
-})
+    //console.log(req.body);
+    const userInfo = JSON.parse(JSON.stringify(req.body, null, 2));
+    if(userInfo.username == 'baraka') {
+        let userData = {
+            name: 'Baraka Mahili',
+            from: 'Congo'
+        }
+        res.status(200).json(userData)
+    } else {
+        res.status(401).json({error: 'You are not authorized'});
+    }
+});
+
+app.get('/getData', (req, res) => {
+    res.status(200).json({name:'Baraka Mahili',from:'Back-End of Flutter'});
+});
 app.listen(PORT, ()=>console.log('Server is listening on port', PORT));
